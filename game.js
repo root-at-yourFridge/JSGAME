1d34bba2b20413375103e2ed2d39a685e48fcb8e
root-at-yourFridge <nettomitarbeiter@gmail.com>
2020-08-18 09:51:53 +0200

    Added support for mobile devices

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 // TODO: Add images for player, box and target
+
 console.log("Game started");
+let startX;
+let startY;
+let endX;
+let endY;
 
 startGame();
 
@@ -26,6 +31,10 @@ function movePlayer(newPlayerRow, newPlayerColumn) {
     playerRow = newPlayerRow;
     playerColumn = newPlayerColumn;
     document.querySelector(`#r${playerRow} > td.c${playerColumn}`).style.backgroundColor = 'orange';
+
+
+    markTarget()
+    checkForWin();
 }
 
 function moveBox(posY, posX) {
@@ -54,15 +63,9 @@ function moveBox(posY, posX) {
         boxColumn += 1;
         document.querySelector(`#r${boxRow} > td.c${boxColumn}`).style.backgroundColor = 'saddlebrown';
     }
-
-    // document.querySelector(`#r${playerRow} > td.c${playerColumn}`).style.backgroundColor = 'white';
-    // playerRow = newPlayerRow;
-    // playerColumn = newPlayerColumn;
-    // document.querySelector(`#r${playerRow} > td.c${playerColumn}`).style.backgroundColor = 'orange';
 }
 
 document.addEventListener('keydown', function (evt) {
-    console.log(evt.key);
     switch (event.key) {
         case "ArrowUp" || "w":
             movePlayer(playerRow - 1, playerColumn);
@@ -77,9 +80,6 @@ document.addEventListener('keydown', function (evt) {
             movePlayer(playerRow, playerColumn + 1);
             break;
     }
-
-    markTarget()
-    checkForWin();
 });
 
 function markTarget() {
@@ -111,3 +111,71 @@ function checkForWin() {
         }
     }
 }
+
+
+// Shamelessly stolen from http://www.javascriptkit.com/javatutors/touchevents2.shtml
+// I tried building this myself, longer than I care to admit :D
+function swipedetect(el, callback){
+  
+    var touchsurface = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 50, //required min distance traveled to be considered swipe
+    restraint = 500, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 500, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function(swipedir){}
+  
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }, true)
+  
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, true)
+  
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if (elapsedTime <= allowedTime){ // first condition for awipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0)? 'a' : 'd' // if dist traveled is negative, it indicates left swipe
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipedir = (distY < 0)? 'w' : 's' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleswipe(swipedir)
+        e.preventDefault()
+    }, true)
+}
+
+swipedetect(document.querySelector('#game'), function(swipedir){
+    // swipedir contains either "none", "left", "right", "top", or "down"
+    switch (swipedir) {
+        case "w":
+            movePlayer(playerRow - 1, playerColumn);
+            break;
+        case "s":
+            movePlayer(playerRow + 1, playerColumn);
+            break;
+        case "a":
+            movePlayer(playerRow, playerColumn - 1);
+            break;
+        case "d":
+            movePlayer(playerRow, playerColumn + 1);
+            break;
+    }
+})
